d29850c94fe4b3e87d2123b6bb191a320c746721
mitchnm <mitchndungu21@gmail.com>
2019-06-20 15:35:25 +0300

    changed database

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'instaproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'mitch',
+    'PASSWORD':'qwertyuiop',
     }
 }
 
