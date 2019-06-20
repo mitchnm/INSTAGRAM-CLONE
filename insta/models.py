2f84df8959f35ff4e3bd7cc6bbc9dabdddfd6f6b
mitchnm <mitchndungu21@gmail.com>
2019-06-20 15:12:58 +0300

    made initial migration

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'instagram/')
+    bio = models.CharField(max_length=250)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'instagram/')
+    image_name = models.CharField(max_length =60)
+    caption = models.CharField(max_length =200)
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField(default=0)
+    comments = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.img_name
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
