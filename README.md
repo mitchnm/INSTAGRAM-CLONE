84cfe36a41aaba14dc8dff776736e8f433c42cf3
mitchnm <mitchndungu21@gmail.com>
2019-06-24 22:02:17 +0300

    created readme

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # INSTAGRAM-CLONE
+
+#### By: **MITCH MUHU**
+
+## Description
+
 This is an instagram clone.
+
+## Features
+* View different photos that interest you.
+* Click on a button to expand to view details of the image.
+* Search for different categories or location of photos and view them.
+
+## Getting Started
+
+* Clone or download the Repo
+* Create a virtual environment
+* Read the specs and requirements files and Install all the requirements.
+* Edit the start.sh file with your secret key
+* Run ./start.py
+* Access the application through `localhost:5000`
+
+## BDD
+
+| Behavior                    | Input                         | Output                       |
+|----------------------------:|:-----------------------------:|-----------------------------:|
+| Homepage loads              |  Click on log in              |  Displays log in page        |
+| New page loads              |  Click on subscribe           |  Displays sign up page       |
+| Profile page loads          |  Click add post               |  Add post                    |
+| Comment page loads          |  Click comments               |  Add comment                 |
+
+## Technologies used
+
+* Python
+* Django Framework
+* HTML, CSS and Bootstrap
+
+## Bugs and issues
+
+If you encounter any problem or issue with the website feel free to contact me at (mitchndungu21@gmail.com)
+
+## License
+
+MIT License
+
+Copyright (c) 2019 mitchnm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- insta/views.py
+++ insta/views.py
@@ -8,79 +8,79 @@ from .forms import NewPostForm, NewProfileForm, CommentForm
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
-  image = Image.objects.all()
-  profile = Profile.objects.all()
-  return render(request, 'index.html', {"image":image, "profile":profile})
+    image = Image.objects.all()
+    profile = Profile.objects.all()
+    return render(request, 'index.html', {"image": image, "profile": profile})
+
 
 @login_required(login_url='/accounts/login/')
 def search_results(request):
     if 'profile' in request.GET and request.GET["profile"]:
         search_term = request.GET.get("profile")
-        searched_profiles = User.objects.filter(username__icontains=search_term)
+        searched_profiles = User.objects.filter(
+            username__icontains=search_term)
         profile = Profile.objects.all()
         message = f"{search_term}"
-        return render(request, 'search.html',{"message":message, "profile":searched_profiles, "profile":profile})
+        return render(request, 'search.html', {"message": message, "profile": searched_profiles, "profile": profile})
     else:
         message = "You haven't searched for any term."
-        return render(request, 'search.html', {"message":message})
+        return render(request, 'search.html', {"message": message})
+
 
 @login_required(login_url='/accounts/login/')
-def profile(request,id):
+def profile(request, id):
     image = Image.objects.filter(user_id=id)
     current_user = request.user
     user = User.objects.get(id=id)
     profile = Profile.objects.all()
     try:
-      profile1 = Profile.objects.filter(name_id=id)
+        profile1 = Profile.objects.filter(name_id=id)
     except ObjectDoesNotExist:
-      return redirect(user.id)
-    return render(request, 'profile.html', {"image":image, "user":user, "profile":profile1, "profile":profile})
+        return redirect(user.id)
+    return render(request, 'profile.html', {"image": image, "user": user, "profile": profile1, "profile": profile})
 
 
 @login_required(login_url='/accounts/login/')
 def new_post(request):
-  current_user = request.user
-  if request.method == 'POST':
-    print('nooooooooooooooooooo')
-    form = NewPostForm(request.POST, request.FILES)
-    if form.is_valid():
-      image = form.save(commit=False)
-      image.user = current_user
-      image.save()
-      return redirect('welcome')
-  else:
-    form = NewPostForm()
-    print('invalid')
-  return render(request, 'post.html', {"form": form})
+    current_user = request.user
+    if request.method == 'POST':
+        print('noo')
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('welcome')
+
+    else:
+        form = NewPostForm()
+        print('xyz')
+    return render(request, 'post.html', {"form": form})
+
 
 @login_required(login_url='/accounts/login/')
-def update_profile(request,id):
-  current_user = request.user
-  user = User.objects.get(id=id)
-  if request.method == 'POST':
-    print('nooooooooooooooooooo')
-    form = NewProfileForm(request.POST, request.FILES)
-    if form.is_valid():
-      profile = form.save(commit=False)
-      profile.username = current_user
-      profile.name_id=current_user.id
-      profile.save()
-      return redirect('profile')
-  else:
-      form = NewProfileForm()
-      print('invalid')
-  return render(request, 'update_profile.html', {"form":form, "user":current_user})
+def update_profile(request, id):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+            profile.name_id = current_user.id
+            profile.save()
+        return redirect('profile')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'update_profile.html', {"form": form, "user": current_user})
+
 
 @login_required(login_url='/accounts/login/')
-def comment(request,id):
-   image =Image.objects.get(profile_id=id)
-   form = CommentForm()
-   try:
-       comments = Comment.objects.filter(comments_id=id)
-   except:
-       form = CommentForm()
-   return render(request,'comment.html',{'image':image,'comments':comments,'form':form})
-
-def form(request):
-    form = NewPostForm()
-    return render(request, 'post.html', {"form":form})
+def comment(request, id):
+    image = Image.objects.get(profile_id=id)
+    form = CommentForm()
+    try:
+        comments = Comment.objects.filter(comments_id=id)
+    except:
+        form = CommentForm()
+    return render(request, 'comment.html', {'image': image, 'comments': comments, 'form': form})
